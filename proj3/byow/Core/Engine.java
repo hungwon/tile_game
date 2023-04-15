c50d6b2971bddd8d87df5d2cc46076e98bc5e8eb
mkim2178 <mkim2178@berkeley.edu>
2023-04-15 11:51:18 -0700

    B -> C

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -46,7 +46,7 @@ public class Engine {
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
 
-        System.out.println("B");
+        System.out.println("C");
 
         TETile[][] finalWorldFrame = null;
         return finalWorldFrame;
