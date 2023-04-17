8b2b0eaabeaa9a8eb96f2acf1d193ba551389413
hungwon <gnsrnjs3626@icloud.com>
2023-04-17 06:35:40 -0700

    1. Constructor changed, 2. toString changed

--- proj3/byow/Core/Block.java
+++ proj3/byow/Core/Block.java
@@ -2,7 +2,6 @@ package byow.Core;
 
 public class Block {
     private String type;
-    private Integer weight;
     private int x;
     private int y;
     private int key; // Index of Block[][] world
@@ -10,8 +9,8 @@ public class Block {
     /**
      *
      * @param index Index of {@code Block[][] world}.
-     * @param x column x of {@code Blcok[][] world}. => x location
-     * @param y row y of {@code Block[][] world}. => y location
+     * @param x x th column of {@code Blcok[][] world}.
+     * @param y y th row of {@code Block[][] world}.
      * @param type type of block. It will be either null, door, room, wall, hallway, avatar
      */
     public Block(int index, int x, int y, String type) {
@@ -19,22 +18,6 @@ public class Block {
         this.x = x;
         this.y = y;
         this.type = type;
-        this.weight = null;
-    }
-
-    /**
-     *
-     * @param index Index of {@code Block[][] world}.
-     * @param x column x of {@code Blcok[][] world}. => x location
-     * @param y row y of {@code Block[][] world}. => y location
-     * @param type type of block. It will be either null, door, room, wall, hallway, avatar
-     */
-    public Block(int index, int x, int y, String type, int weight) {
-        key = index;
-        this.x = x;
-        this.y = y;
-        this.type = type;
-        this.weight = weight;
     }
 
     public int Key() {
@@ -49,21 +32,25 @@ public class Block {
         return y;
     }
 
-    public double Weight() {
-        return weight;
-    }
-
-    /** blockType() returns current block's type.
-     * This method is useful when we want to check the type of the current block.
-     * */
+    /**
+     * <h2>
+     *     {@code type} of this Block
+     * </h2>
+     * @return {@code type} of {@code this} block
+     */
     private String blockType() {
         return type;
     }
 
 
-    /** toString() is for debugging purpose. Example String Format -> "Block Type: Floor / Weight: 5" */
+    /**
+     * <p>
+     *     Ex) "(Key: 10, Type: Floor)"
+     * </p>
+     * @return String ver of {@code this} block
+     */
     @Override
     public String toString() {
-        return "Block Type: " + type + " / " + "Weight: " + weight;
+        return "(Key: " + key + ", Type: " + type + ")";
     }
 }
